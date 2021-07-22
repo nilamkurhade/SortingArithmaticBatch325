d8b3c146bcca87c27386608fba932de23cdb086d
nilamkurhade <nilamkurhade87@gmail.com>
2021-07-22 10:42:42 +0530

    computed a+b*c

--- SortingArithmatic.sh
+++ SortingArithmatic.sh
@@ -4,3 +4,6 @@ echo "Welcome to sorting arithmatic problem statement"
 read -p "Enter value for a: " a
 read -p "Enter value for b: " b
 read -p "Enter value for c: " c
+
+result=$(($a+$b*$c))
+echo $result
